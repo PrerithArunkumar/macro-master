bd3ba1a26bec775da1fb0b7cb051253217f034ff
manntalati <mann.talati@gmail.com>
2025-03-01 19:41:21 -0600

    Setting up SpringBoot and ReactNative libraries

---
+++ Macro-Masters/src/main/java/com/example/Macro_Masters/MacroMastersApplication.java
@@ -0,0 +1,13 @@
+package com.example.Macro_Masters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacroMastersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacroMastersApplication.class, args);
+	}
+
+}
---
+++ Macro-Masters/src/test/java/com/example/Macro_Masters/MacroMastersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Macro_Masters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacroMastersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
